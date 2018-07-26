5edf221e9a354ecb35a7ce89f234e7efabedca73
orange <965526262@qq.com>
2018-07-26 19:08:25 +0800

    2018.07.26--Redis数据类型String、Hash、List、Set、Zset操作测试

--- src/main/java/com/zyj/springboot/demo/core/RedisConfig.java
+++ src/main/java/com/zyj/springboot/demo/core/RedisConfig.java
@@ -8,8 +8,9 @@ import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.*;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -30,17 +31,32 @@ public class RedisConfig extends CachingConfigurerSupport {
         template.setConnectionFactory(connectionFactory);
 
         //使用Jackson2JsonRedisSerializer来序列化和反序列化redis的value值
-        Jackson2JsonRedisSerializer serializer = new Jackson2JsonRedisSerializer(Object.class);
+        Jackson2JsonRedisSerializer jackson2Serializer = new Jackson2JsonRedisSerializer(Object.class);
 
         ObjectMapper mapper = new ObjectMapper();
+        // 指定要序列化的域，field,get和set,以及修饰符范围，ANY是都有包括private和public
         mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        // 指定序列化输入的类型，类必须是非final修饰的，final修饰的类，比如String,Integer等会跑出异常
         mapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
-        serializer.setObjectMapper(mapper);
+        jackson2Serializer.setObjectMapper(mapper);
 
-        template.setValueSerializer(serializer);
+        // 值采用json序列化
+        //template.setValueSerializer(jackson2Serializer);
         //使用StringRedisSerializer来序列化和反序列化redis的key值
-        template.setKeySerializer(new StringRedisSerializer());
+        //template.setKeySerializer(template.getStringSerializer());
+        // 设置hash key 和value序列化模式
+        //template.setHashValueSerializer(jackson2Serializer);
+        //template.setHashKeySerializer(template.getStringSerializer());
+
+        //在使用String的数据结构的时候使用这个来更改序列化方式
+        RedisSerializer<String> stringSerializer = new StringRedisSerializer();
+        template.setValueSerializer(stringSerializer);
+        template.setKeySerializer(stringSerializer);
+        template.setHashValueSerializer(stringSerializer);
+        template.setHashKeySerializer(stringSerializer);
+
         template.afterPropertiesSet();
+
         return template;
     }
 }
--- src/test/java/com/zyj/springboot/demo/RedisCacheTest.java
+++ src/test/java/com/zyj/springboot/demo/RedisCacheTest.java
@@ -3,21 +3,107 @@ package com.zyj.springboot.demo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.data.redis.core.*;
 import org.springframework.test.context.junit4.SpringRunner;
 import javax.annotation.Resource;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class RedisCacheTest {
+
     @Resource
-    private RedisTemplate<String,Object> redisTemplate;
+    private RedisTemplate<String, Object> redisTemplate;
 
     @Test
-    public void cacheTest(){
-        ValueOperations<String,Object> valueOperations = redisTemplate.opsForValue();
-        valueOperations.set("hello", "Redis");
-        System.out.println("RedisHello: " + valueOperations.get("hello"));
+    public void cacheValueTest(){
+        ValueOperations<String, Object> valueOperations = redisTemplate.opsForValue();
+        //设置key为hello到Redis
+        valueOperations.set("hello", "Hello Redis!!!", 1, TimeUnit.HOURS);
+
+        System.out.println("ValueHello: " + valueOperations.get("hello"));
+    }
+
+    @Test
+    public void cacheHashTest(){
+        HashOperations<String, String, Object> hashOperations = redisTemplate.opsForHash();
+        Map<String, Object> appleMap = new HashMap<>();
+        appleMap.put("sn", "201807260002");
+        appleMap.put("name", "橘子");
+        appleMap.put("type", "香橘");
+        appleMap.put("area", "上海青浦");
+        //appleMap的key和value全部插入appleHash中
+        hashOperations.putAll("appleHash", appleMap);
+        //单个插入appleHash中key和value值
+        //hashOperations.put("appleHash", "sn", "201807260002");
+        //hashOperations.put("appleHash", "name", "橘子");
+        //hashOperations.put("appleHash", "type", "香橘");
+        //hashOperations.put("appleHash", "area", "上海青浦");
+        System.out.println("HashAplleName: " + hashOperations.get("appleHash", "name"));
+    }
+
+    @Test
+    public void cacheeListTest(){
+        ListOperations<String, Object> listOperations = redisTemplate.opsForList();
+        List<Object> list = new ArrayList<>();
+        list.add("java");
+        list.add("python");
+        list.add("c#");
+        list.add("c++");
+        list.add("php");
+        //从list集合中按左侧顺序加入元素到listObject集合
+        listOperations.leftPushAll("listObject", list);
+        //在listObject集合左侧加入元素scala
+        //listOperations.leftPush("listObject", "scala");
+        //在listObject集合中c++左侧加入元素javaScript
+        //listOperations.leftPush("listObject", "c++", "javaScript");
+        //在listObject集合中c++右侧加入元素ruby
+        //listOperations.rightPush("listObject", "c++", "ruby");
+        System.out.println("ListString: " + listOperations.range("listObject", 0, -1));
+    }
+
+    @Test
+    public void cacheSetTest(){
+        SetOperations<String, Object> setOperations = redisTemplate.opsForSet();
+        //languageSet无序集合加入多个元素
+        setOperations.add("languageSet", "Java", "Python", "C++", "Scala");
+        setOperations.add("languageSet", "Rudy");
+
+        //languageSet2无序集合加入多个元素
+        setOperations.add("languageSet2", "JavaScript", "Python", "Java", "C#");
+
+        // 求languageSet和languageSet2交集存入languageSet3
+        setOperations.intersectAndStore("languageSet", "languageSet2", "languageSet3");
+
+        // 求languageSet和languageSet2并集存入languageSet4
+        setOperations.unionAndStore("languageSet", "languageSet2", "languageSet4");
+
+        System.out.println("Set-languageSet:" + setOperations.members("languageSet"));
+        System.out.println("Set-languageSet2:" + setOperations.members("languageSet2"));
+        System.out.println("Set-languageSet3:" + setOperations.members("languageSet3"));
+        System.out.println("Set-languageSet4:" + setOperations.members("languageSet4"));
+    }
+
+    @Test
+    public void cacheZsetTest(){
+        ZSetOperations<String, Object> zSetOperations = redisTemplate.opsForZSet();
+        //单个插入Set有序集合
+        zSetOperations.add("stringZset", "zset-1", 4);
+        zSetOperations.add("stringZset", "zset-2", 3);
+        zSetOperations.add("stringZset", "zset-3", 2);
+        zSetOperations.add("stringZset", "zset-4", 1);
+
+        //批量插入Set有序集合
+        ZSetOperations.TypedTuple<Object> typedTuple = new DefaultTypedTuple<>("zset-5", 3.2);
+        ZSetOperations.TypedTuple<Object> typedTuple1 = new DefaultTypedTuple<>("zset-6", 1.9);
+        Set<ZSetOperations.TypedTuple<Object>> tuples = new HashSet<>();
+        tuples.add(typedTuple);
+        tuples.add(typedTuple1);
+        zSetOperations.add("stringZset", tuples);
+
+        System.out.println("Zset-stringZset:" + zSetOperations.range("stringZset", 0, -1));
+        //System.out.println("Zset-stringZset:" + zSetOperations.incrementScore("stringZset", "zset-1", 1.1));
+        //System.out.println("Zset-stringZset:" + zSetOperations.rangeByScore("stringZset",0, 4, 1, 2));
     }
 }
