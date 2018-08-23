9e89af1acfbecf6995b4dbe3203ba104ee8a46a6
orange <965526262@qq.com>
2018-08-23 18:28:33 +0800

    修改使用slf4j日志

--- src/main/java/com/zyj/springboot/demo/DemoApplication.java
+++ src/main/java/com/zyj/springboot/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-@MapperScan("com.zyj.springboot.demo.dao")
+@MapperScan(basePackages = "com.zyj.springboot.demo.dao")
 public class DemoApplication extends SpringBootServletInitializer {
 
     @Override
